07545e8096e0491367fa9b8333aba8d085250065
Gu1Assis <assis.guisilva@gmail.com>
2025-12-19 13:07:47 -0300

    initial java commit

---
+++ Java/demo/src/main/java/com/primeirospring/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.primeirospring.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.primeirospring.demo.service.ConsumoApi;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoApi api = new ConsumoApi();
+		String responseJson = api.obterDados("https://viacep.com.br/ws/01001000/json/");
+		System.out.println(responseJson);
+	}
+
+}
